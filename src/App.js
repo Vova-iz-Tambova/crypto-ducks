568d51d9dea1ebc4ffd5e1e4e6ffb75a718d62d9
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-08-11 22:31:39 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+// импортируем компоненты приложения
+import Login from './Login.js';
+import Register from './Register.js';
+import Ducks from './Ducks.js';
+import MyProfile from './MyProfile.js';
+// импортируем CSS
+import './styles/App.css';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loggedIn: false
+    }
+  } 
+
+  render(){
+    return (
+      <Routes>
+        <Route path="/" element={this.state.loggedIn ? <Navigate to="/ducks" replace /> : <Navigate to="/login" replace />} /> 
+        <Route path="/ducks" element={<Ducks/>} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/login" element={
+          <div className="loginContainer">
+            <Login  />
+          </div>} />
+        <Route path="/register" element={
+          <div className="registerContainer">
+            <Register />
+          </div>} />
+      </Routes>
+    )
+  }
 }
 
 export default App;
