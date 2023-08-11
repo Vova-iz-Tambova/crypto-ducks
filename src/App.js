d1cf894249f28b687dfabf3d2f40a4815e5ddf15
Vova-iz-Tambova <voller.ruden@yandex.ru>
2023-08-11 22:46:49 +0300

    add ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
 // App.js
 
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-// импортируем компоненты приложения
-import Login from './Login.js';
-import Register from './Register.js';
-import Ducks from './Ducks.js';
-import MyProfile from './MyProfile.js';
-// импортируем CSS
-import './styles/App.css';
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Login from "./Login.js";
+import Register from "./Register.js";
+import Ducks from "./Ducks.js";
+import MyProfile from "./MyProfile.js";
+import ProtectedRoute from "./ProtectedRoute"; // импортируем HOC
+import "./styles/App.css";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      loggedIn: false
-    }
-  } 
+      loggedIn: false,
+    };
+  }
 
-  render(){
+  render() {
     return (
       <Routes>
-        <Route path="/" element={this.state.loggedIn ? <Navigate to="/ducks" replace /> : <Navigate to="/login" replace />} /> 
-        <Route path="/ducks" element={<Ducks/>} />
-        <Route path="/my-profile" element={<MyProfile />} />
-        <Route path="/login" element={
+        <ProtectedRoute
+          path="/ducks"
+          loggedIn={this.state.loggedIn}
+          component={Ducks}
+        />
+        <ProtectedRoute
+          path="/my-profile"
+          loggedIn={this.state.loggedIn}
+          component={MyProfile}
+        />
+        <Route path="/login">
           <div className="loginContainer">
-            <Login  />
-          </div>} />
+            <Login handleLogin={this.handleLogin} />
+          </div>
+        </Route>
+        <Route path="/register">
+          <div className="registerContainer">
+            <Register />
+          </div>
+        </Route>
+        <Route path="/" element={this.state.loggedIn ? <Navigate to="/ducks" replace /> : <Navigate to="/login" replace />} />
+        <Route path="/ducks" element={<ProtectedRouteElement element={<Ducks/>} loggedIn={this.state.loggedIn}/>} />
+        <Route path="/my-profile" element={<ProtectedRouteElement element={<MyProfile/>} loggedIn={this.state.loggedIn}/>} />
         <Route path="/register" element={
           <div className="registerContainer">
             <Register />
           </div>} />
+        <Route path="/login" element={
+          <div className="loginContainer">
+            <Login handleLogin={this.handleLogin} />
+          </div>} />
       </Routes>
-    )
+    );
   }
 }
 
